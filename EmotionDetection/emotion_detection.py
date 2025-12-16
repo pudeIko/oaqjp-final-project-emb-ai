3ab5d7c1b2ffb5f11757997c968618640b972058
wrp <wojciech.pudelko@gmail.com>
2025-12-16 01:55:49 -0500

    final version

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -10,34 +10,25 @@ def emotion_detector(text_to_analyse):
     # Sending a POST request
     resp = requests.post(url, json=myobj, headers=header)
 
+    output = {}
+    output["anger"] = None
+    output["disgust"] = None
+    output["fear"] = None
+    output["joy"] = None
+    output["sadness"] = None
+    output["dominant_emotion"] = None
+
     # Parsing the JSON response from the API
-    formatted = json.loads(resp.text)
+    if resp.status_code == 200:
+        formatted = json.loads(resp.text)
+        emotions = formatted["emotionPredictions"][0]["emotion"]
+        dominant_val = 0
+        for emotion in emotions.keys():
+            output[emotion] = emotions[emotion]
+            if emotions[emotion] > dominant_val:
+                output["dominant_emotion"] = emotion
+                dominant_val = emotions[emotion]
+        return output
+    elif resp.status_code == 400:
+        return output
 
-    emotions = formatted["emotionPredictions"][0]["emotion"]
-    dominant_val = 0
-    for emotion in emotions.keys():
-        if emotions[emotion] > dominant_val:
-            dominant_emotion = emotion
-            dominant_val = emotions[emotion]
-
-    emotions["dominant_emotion"] = dominant_emotion
-
-    return emotions
-
-    # label = formatted['documentSentiment']['label']
-    # score = formatted['documentSentiment']['score']
-
-    # Returning a dictionary
-    # return {'label': label, 'score': score}
-
-
-
-
-    # # If the response status code is 200, extract the label and score from the response
-    # if response.status_code == 200:
-    #     label = formatted_response['documentSentiment']['label']
-    #     score = formatted_response['documentSentiment']['score']
-    # # If the response status code is 500, set label and score to None
-    # elif response.status_code == 500:
-    #     label = None
-    #     score = None
--- server.py
+++ server.py
@@ -1,50 +1,42 @@
+"""server"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 # init the flask app
-emotionDetector = Flask("Sentiment Analyzer")
+EMOTION_DETECTOR = Flask("Sentiment Analyzer")
 
-@emotionDetector.route("/emotionDetector")
+@EMOTION_DETECTOR.route("/emotionDetector")
 def send_detector():
+    """send_detector function."""
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # process the input
-    # res = emotion_detector(text_to_analyze)
-    # ang = res["anger"]
-    # dis = res["disgust"]
-    # fea = res["fear"]
-    # joy = res["joy"]
-    # sad = res["sadness"]
-    # dom = res["dominant_emotion"]
+    res = emotion_detector(text_to_analyze)
+    ang, dis, fea = res["anger"], res["disgust"], res["fear"]
+    joy, sad, dom = res["joy"], res["sadness"], res["dominant_emotion"]
 
-    output_textx = (
-        f"For the given statement, the system response is "
-        f" 'anger': {res["anger"]}, "
-        f"'disgust': {res["disgust"]}, "
-        f"'fear': {res["fear"]}, "
-        f"'joy': {res["joy"]} and "
-        f"'sadness': {res["sadness"]}. "
-        f"The dominant emotion is {res["dominant_emotion"]}."
-    )
+    # Return a formatted string
+    if dom is None:
+        output_text = "Invalid text! Please try again!"
+    else:
+        output_text = (
+            f"For the given statement, the system response is "
+            f"'anger': {ang}, "
+            f"'disgust': {dis}, "
+            f"'fear': {fea}, "
+            f"'joy': {joy} and "
+            f"'sadness': {sad}. "
+            f"The dominant emotion is {dom}."
+        )
+    return output_text
 
-    # Return a formatted string with the sentiment label and score
-    return output_textx
 
-@emotionDetector.route("/")
+@EMOTION_DETECTOR.route("/")
 def render_index_page():
+    """render index page"""
     return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-
-
-
-    # # Check if the label is None, indicating an error or invalid input
-    # if label is None:
-    #     return "Invalid input! Try again."
-    # else:
-    #     # Return a formatted string with the sentiment label and score
-    #     return output_textx
+    EMOTION_DETECTOR.run()
